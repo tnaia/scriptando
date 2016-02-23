cdbae1ae087b3f4e488d56ee321e2a7121cab93a
Tássio Naia <tnaia.@member.fsf.org>
2016-02-23 19:14:13 +0000

    breaking long fasta lines (does not break headers)

---
+++ break-to-50.py
@@ -0,0 +1,31 @@
+#!/bin/python
+# -*- coding: utf-8 -*-
+
+# Breaks lines longer than 50 characters,
+# except those starting by a '>' (greater sign).
+#
+# Copyright 2016 Tássio Naia
+#
+# This is free software, licensed under GPL v3 or later.
+
+import sys
+
+filename = sys.argv[-1]
+fin = open(filename)
+
+max_len = 5
+
+for line in fin:
+    l = line.rstrip()
+    if l[0] == '>':
+        print(l)
+    else:
+        line_len = len(l)
+        index = 0
+        while line_len -index > max_len:
+            print(l[index:index+max_len])
+            index += max_len
+        if index < line_len:
+            print(l[index:])
+    
+fin.close()
