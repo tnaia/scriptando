660d3f2a3ca9d2aa1c90f05f8172de1bcdb26f47
Tássio Naia <tnaia@member.fsf.org>
2018-04-22 13:45:05 +0200

    which tex files in a folder include other files? -- pretty raw version

--- tex-inclusion-digraph.py
+++ tex-inclusion-digraph.py
@@ -1,17 +1,32 @@
 #! /usr/bin/python
+# Files included by each tex file.
+# tns@polignu.org
 
 # Basic use (requires `graphviz`'s program `dot`:
 # If `~/path/to/folder/` contains the texfiles,
 # and you want an image called `tex-inclusion.png`,
 # you should run
 #
-# python tex-inclusion-digraph.py ~/path/to/folder/ > dot -Tpng -o tex-inclusion.png
+# python tex-inclusion-digraph.py ~/path/to/folder/ | dot -Tpng -o tex-inclusion.png
 #
 # Note: This will put the image in the current folder!
+
+# Increase whenever this file is changed!
+version="0.5"
+
+# CHANGELOG
+# - v0.5 node names can contain double quotes!
+# - v0.4 error message if wrong number of argumenst
+# - v0.3 edges have line numbers
+# - v0.2 node names can contain anything but double quotes
+# - v0.1 it's alive!
+
+
 from os import listdir
 from os.path import isfile, join
 import re
 import sys
+
 
 # I am looking for occurences of
 # - \input{X}
@@ -24,6 +39,11 @@ def includes_file(line):
     p = re.compile('\\\\(include|input|bibliography)[^\{]*\{([^\}]+)\}')
     return p.search(line)
 
+if len(sys.argv) != 2:
+    print("Check which texfiles in a folder include other files.\nVersion " + version)
+    print("Usage: texinclusion <PATH>\n" % sys.argv[0])
+    exit(1)
+    
 mypath = sys.argv[1]
 print("// mypath is", mypath)
       
@@ -32,10 +52,10 @@ texfiles = [f for f in listdir(mypath) if isfile(join(mypath, f)) and f[-4:] == 
 print("digraph texinclusion{")
 for f in texfiles:
     fin = open(join(mypath,f))
-    f_clean = f #was f.replace('-','_').replace('.','_')[:-4]
+    f_clean = f.replace('"','\\"')
     for linum,l in enumerate(fin):
         m = includes_file(l)
         if m:
-            included_file = m.group(2) #was m.group(2).replace('-','_').replace('.','_').replace('/','7')
-            print ('    %40s -> "%s:%d"; // %s' % ('"'+f_clean+'"', included_file, linum, m.group(0)))
+            included_file = m.group(2)
+            print ('    %40s -> "%s" [label="%d"]; // %s' % ('"'+f_clean+'"', included_file, linum, m.group(0)))
 print("}")
